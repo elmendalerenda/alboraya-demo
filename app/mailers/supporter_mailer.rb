a932394cea50b95b0a9887a53127c5bf98875a59
Miguel Angel <miguelangel@peertransfer.com>
2015-09-18 18:22:01 +0000

    fix tests

--- app/mailers/supporter_mailer.rb
+++ app/mailers/supporter_mailer.rb
@@ -1,25 +1,25 @@
 class SupporterMailer < ApplicationMailer
   default from: 'no-reply@suggestion-box.com'
- 
+
   def info_new_comment(suggestion, comment, email)
     @suggestion = suggestion
     @comment = comment
     @url  = get_host + "suggestions/#{@suggestion.id}"
     mail(to: email, subject: 'Suggestion Box - New Comment')
   end
-  
+
   def info_for_supporters(comment)
     @suggestion = comment.suggestion
     @comment = comment
     @url  = get_host + "suggestions/#{@suggestion.id}"
     mail(to: @comment.email, subject: 'Suggestion Box - Thanks for your support')
   end
-  
+
   private
-  
+
   def get_host
     if Rails.env == "production"
-      return "https://tfg-suggestion-box.herokuapp.com/"
+      return ENV['APP_HOST']
     else
       return "http://localhost:3000/"
     end
--- spec/models/white_list_email_spec.rb
+++ spec/models/white_list_email_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe WhiteListEmail, type: :model do
 
   describe '#in_whitelist?' do
-    it 'returns always true if env is not production' do
+    it 'returns always true if env is development' do
       expect(Rails).to receive(:env).and_return('development')
 
       expect(described_class.in_whiteList?('any string')).to eql true
